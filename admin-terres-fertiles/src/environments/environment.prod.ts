2e3f0511509e6e84d9f8c4a8957f7ec8e833cd7e
Ismaila SACKO <ismalsacko@yahoo.fr>
2025-12-15 13:29:08 +0100

    Responsive ds page de fiches agro pedo

--- admin-terres-fertiles/src/environments/environment.prod.ts
+++ admin-terres-fertiles/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://terres-fertiles.ismael-dev.com/api/', // API Django en prod
+  //apiUrl: 'https://terres-fertiles.ismael-dev.com/api/', // API Django en prod
+  apiUrl: 'https://api.terres-fertiles.com/api/'
+  
 };
--- admin-terres-fertiles/src/environments/environment.ts
+++ admin-terres-fertiles/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:8000/api/'
-  apiUrl: 'https://api.terres-fertiles.com/api/'
+  apiUrl: 'http://localhost:8000/api/'
+  //apiUrl: 'https://api.terres-fertiles.com/api/'
 }; 
