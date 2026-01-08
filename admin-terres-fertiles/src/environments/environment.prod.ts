91b2001290da19ba8d86334d1cdd12c73bb44cab
Ismaila Sacko <ismalsacko@yahoo.fr>
2026-01-08 19:49:40 +0100

    conflit entre le domaine temporire et le vrai domaine

--- admin-terres-fertiles/src/environments/environment.prod.ts
+++ admin-terres-fertiles/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.terres-fertiles.com/api/'
-  
+  //apiUrl: 'https://api.terres-fertiles.com/api/' -- url pour le vrai domaine
+  apiUrl: 'https://terres-fertiles.ismael-dev.com/api/' // url pour le domaine de dev
+
 };
