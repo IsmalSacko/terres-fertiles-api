783af77c1616d6b87a049eed7855cd1b8446cb9d
Ismaila Sacko <ismalsacko@yahoo.fr>
2026-01-09 20:11:29 +0100

    correction de bug lie à la modification d'un melange

--- admin-terres-fertiles/src/environments/environment.prod.ts
+++ admin-terres-fertiles/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://api.terres-fertiles.com/api/' // url pour le vrai domaine
-  //apiUrl: 'https://terres-fertiles.ismael-dev.com/api/' // url pour le domaine de dev
+  //apiUrl: 'https://api.terres-fertiles.com/api/' -- url pour le vrai domaine
+  apiUrl: 'https://terres-fertiles.ismael-dev.com/api/' // url pour le domaine de dev
 
   //apiUrl: 'https://api.terres-fertiles.com/api/'
 
